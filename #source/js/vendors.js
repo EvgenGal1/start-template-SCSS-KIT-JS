cf395b4d6d43816364bd311fb90669d48e15bfde
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2021-03-07 12:28:51 +0300

    > 1:10:10 popover.min

--- #source/js/vendors.js
+++ #source/js/vendors.js
@@ -2,4 +2,5 @@
 //@prepros-append jquery.inputmask.bundle.min.js
 //@prepros-append jquery.nicescroll.min.js
 //@prepros-append jquery.ui.touch-punch.min.js
+//@prepros-append jquery.popover.min.js
 //@prepros-append baguetteBox.js
